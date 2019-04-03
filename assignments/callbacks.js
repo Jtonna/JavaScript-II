5fc311f5582af32a614c544f0b2e0ca009404006
Jtonna <34466548+Jtonna@users.noreply.github.com>
2019-04-02 17:53:05 -0700

    summary

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,38 +1,50 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work.
+// You have been provided an example of a problem and a solution to see how this works with our items array.
+// Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+/* #region -- EXAMPLE REGION   */
 
-/* 
+// const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+// //Given this problem: 
+// function firstItem(arr, cb) {
+//   // firstItem passes the first item of the given array to the callback function.
+// }
+// // Potential Solution:
+// // Higher order function using "cb" as the call back
+// function firstItem(arr, cb) {
+//   return cb(arr[0]);
+// }
+// // Function invocation 
+// firstItem(items, function (first) {
+//   console.log(first)
+// });
 
-  //Given this problem: 
-  
-  function firstItem(arr, cb) {
-    // firstItem passes the first item of the given array to the callback function.
-  }
+/* #endregion */
 
-  // Potential Solution:
+const itemsV2 = ['GTX 1060', 'i7 6600k', 'Trident 8GB DDR5', 'Coolermaster Case'];
+// My Custom Array
 
-  // Higher order function using "cb" as the call back
-  function firstItem(arr, cb) {
-    return cb(arr[0]);
-  }
-
-  // Function invocation 
-  firstItem(items, function(first) {
-    console.log(first)
-  });
-
-*/
-
-
-function getLength(arr, cb) {
+function getLength(array, callback) {
   // getLength passes the length of the array into the callback.
+  console.log(callback(array))
 }
+let arrayLength = function (itemsV2) {
+  return itemsV2.length
+}
+getLength(itemsV2, arrayLength);
 
-function last(arr, cb) {
+// end problem 1
+
+// start problem 2
+function last(array, cb) {
   // last passes the last item of the array into the callback.
+  console.log(itemsV2.length)
 }
 
+
+// end problem 2
+
+//start problem 3
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
 }
